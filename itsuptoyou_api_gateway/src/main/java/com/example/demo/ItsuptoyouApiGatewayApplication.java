b6f3fb3a60f63a4009f53c329e9684daef19ec0b
angelo <angelods18@gmail.com>
2022-03-02 21:52:03 +0100

    create project

---
+++ itsuptoyou_api_gateway/src/main/java/com/example/demo/ItsuptoyouApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsuptoyouApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsuptoyouApiGatewayApplication.class, args);
+	}
+
+}
