e9c580f2aff5efc67a567d662e96f12d44e6ec01
angelo <angelods18@gmail.com>
2022-03-09 22:32:31 +0100

    add zuul for gateway to other services

--- itsuptoyou-api-gateway/src/main/java/it/itsuptoyou/ItsuptoyouApiGatewayApplication.java
+++ itsuptoyou-api-gateway/src/main/java/it/itsuptoyou/ItsuptoyouApiGatewayApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -24,6 +25,7 @@ import it.itsuptoyou.repository.AuthorityRepository;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class ItsuptoyouApiGatewayApplication {
 
 	@Autowired
